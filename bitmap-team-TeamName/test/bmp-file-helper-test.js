e0563beb5f84dd606abde9abbbc97ea33352fdb1
Nikko Pisciotti <t.bird.13@hotmail.com>
2017-02-17 11:47:42 -0800

    first file helper test written

--- bitmap-team-TeamName/test/bmp-file-helper-test.js
+++ bitmap-team-TeamName/test/bmp-file-helper-test.js
@@ -2,17 +2,22 @@
 
 const expect = require('chai').expect;
 const fs = require('fs');
-const fileReadHelper = require(`${__dirname}/../lib/bmp-file-helper.js`);
+// const fileReadHelper = require(`${__dirname}/../lib/bmp-file-helper.js`);
 
-describe('Bit Map Reader', function() {
+// var testFunc = function(data) {
+//   console.log(data);
+// };
+
+describe('bmp-file-helper Module', function() {
   describe('Expecting readFile to output a buffer.', function() {
     it('should be a pseudo-object ', function(done) {
-       fileReadHelper.readFile(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data) {
-         expect(err).to.equal(null);
-         expect(typeof data).to.equal('object');
-         done();
-       });
+      fs.readFile(`${__dirname}/../img/palette-bitmap.bmp`, function(err, data) {
+        expect(err).to.equal(null);
+        expect(typeof data).to.equal('object');
+        done();
+      });
     });
   });
-  describe('')
+  describe('Expecting readBitMap to handle asynch callback correctly', function() {
+  });  
 });
