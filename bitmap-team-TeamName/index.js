502cded1f712500fc8e4d4c708143b19e1844734
radenska <yana_radenska@yahoo.com>
2017-02-17 15:27:57 -0800

    added cli interface

--- bitmap-team-TeamName/index.js
+++ bitmap-team-TeamName/index.js
@@ -4,4 +4,10 @@ const fileReadHelper = require(`${__dirname}/lib/bmp-file-helper.js`);
 const bmpCon = require(`${__dirname}/model/bmp-constructor.js`);
 const makeBitMap = bmpCon.makeBitMap;
 
+if (!process.argv[2] || !process.argv[3]) {
+  console.error('Please enter two parameters: file name and transformation type (grayscale, invert, or red)');
+  console.log('Format: node index <filename> <transformation type> Example: node index color-palette.bmp invert');
+  throw('error');
+}
+
 fileReadHelper.getBitMap(makeBitMap);
