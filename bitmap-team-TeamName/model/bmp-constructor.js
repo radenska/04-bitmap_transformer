ced7e3d4c1fecae5aee4951fc9355939b946d8b2
Nikko Pisciotti <t.bird.13@hotmail.com>
2017-02-17 16:03:58 -0800

    added large change in modularity, both prepare/package array functions are now on the bmp-constructor file, color transformations can be called depending on which one is wanted

--- bitmap-team-TeamName/model/bmp-constructor.js
+++ bitmap-team-TeamName/model/bmp-constructor.js
@@ -1,16 +1,49 @@
 'use strict';
 
-module.exports = exports = {};
 const colorCon = require(`${__dirname}/color-constructor.js`);
+const fileReadHelper = require(`${__dirname}/../lib/bmp-file-helper.js`);
+module.exports = exports = {};
 
+//create a place for our constructed bitmap object
+exports.bmObj = {};
+
+//create an object for the transformed buffer to be stored as (on the module)
+exports.transformedBitMap = {};
+
+//create an array for each transformation to store the updated buffer string
+exports.bmObj.transformedArray = [];
+
+//Here is the the pseudo-code below that brian wrote for me. He suggested we look at exposing only BitMap constructor to the module from this file.
+
+// function BitmapConstructor(bm) {
+//   // house bitmap data
+// }
+//
+// BitmapConstructor.prototype.makeBitmap = function() {
+//   // logic here
+//   return this;
+// }
+//
+// var Bitmap = module.exports = BitmapConstuctor;
+//
+// // new file
+// var BM = require('./lib/bm-constructor');
+//
+// var bitmap = new BM();
+//
+// bitmap.makeBitmap();
 
 exports.makeBitMap = function(data) {
   exports.bmp = data;
   exports.bmObj = new exports.BitMap(data);
-  colorCon.changeColor();
+  prepareArray(exports.bmObj);
+  colorCon.greyscale(exports.bmObj.preparedArray);
+  packageArray(exports.bmObj.transformedArray);
+  fileReadHelper.newBitMap(exports.transformedBitMap);
+  // colorCon.changeColor();
 };
 
-exports.BitMap = function(data) {
+function BitMap = function(data) {
   this.type = data.toString('utf-8', 0, 2);
   this.totalFileSize = data.readInt32LE(2);
   this.arrayLoc = data.readInt32LE(10);
@@ -20,6 +53,25 @@ exports.BitMap = function(data) {
   this.bitsPerPixel = data.readInt32LE(28);
   this.horizRes = data.readInt32LE(38);
   this.verticRes = data.readInt32LE(42);
-  // this.colorTableStart = data.readInt32LE(66);
-  this.allColorTable = data.toString('hex', 66, 1090);
+  this.colorTable = data.toString('hex', 66, 1090);
 };
+
+function prepareArray(bmObj) {
+  var colorTableArray = bmObj.colorTable.split(''); //split to modify string of hex chars
+  exports.bmObj.preparedArray = [];
+  for (let i = 0; i <= colorTableArray.length - 2; i += 2) {
+    exports.bmObj.preparedArray.push(colorTableArray[i] + colorTableArray[i+1]);
+  }
+}
+
+function packageArray(transformedArray) {
+  var packagedArray = [];
+  for (let i = 0; i <= transformedArray.length - 4; i += 4) {
+    packagedArray.push(transformedArray[i] + transformedArray[i+1] + transformedArray[i+2]);
+  }
+  var offset = 66;
+  for (let i = 0; i < packagedArray.length; i++) {
+    exports.transformedBitMap.write(packagedArray[i], offset, 'hex');
+    offset += 4;
+  }
+}
--- bitmap-team-TeamName/model/color-constructor.js
+++ bitmap-team-TeamName/model/color-constructor.js
@@ -4,47 +4,52 @@ module.exports = exports = {};
 const bmpCon = require(`${__dirname}/bmp-constructor.js`);
 const fileReadHelper = require(`${__dirname}/../lib/bmp-file-helper.js`);
 
-exports.changeColor = function() { //just inverting colors right now
-  var tableArray = bmpCon.bmObj.allColorTable.split('');
-  //       1. combine all values in pairs
-  var newArray = [];
-  for (let i = 0; i <= tableArray.length - 2; i += 2) {
-    newArray.push(tableArray[i] + tableArray[i+1]);
-  }
-  //        2.turn hex string values to decimal
-  //        4. subtract each RGB value from 255, leave 7th and 8th positions as is;
-  //        5. turn back into hex
-  // newArray.forEach((val, i) => { //invert colors
-  //   newArray[i] = (255 - parseInt(val, 16)).toString(16);
-  //   if (newArray[i].length === 1) newArray[i] = '0' + newArray[i];
-  // });
-  // newArray.forEach((val, i) => { //grayscale colors
-  //   newArray[i] = Math.ceil((parseInt(val, 16))*0.7);
-  //   if (newArray[i] > 255) newArray[i] = 255;
-  //   newArray[i] = newArray[i].toString(16);
-  //   if (newArray[i].length === 1) newArray[i] = '0' + newArray[i];
-  // });
-  for (let i = 2; i <= newArray.length - 4; i += 4) { //change blue values
-    newArray[i] = Math.floor((parseInt(newArray[i], 16))*1.7);
-    if (newArray[i] > 255) newArray[i] = 255;
-    newArray[i] = newArray[i].toString(16);
-    if (newArray[i].length === 1) newArray[i] = '0' + newArray[i];
-  }
-  tableArray = [];
-  for (let i = 0; i <= newArray.length - 4; i += 4) {
-    tableArray.push(newArray[i] + newArray[i+1] + newArray[i+2]);
-  }
-  //        6. combine 3 of the pairs of 4 (to form 6 bytes)
-  var offset = 66;
-  for (let i = 0; i < tableArray.length; i++) {
-    bmpCon.bmp.write(tableArray[i], offset, 'hex');
-    offset += 4;
-  }
-  //        8. place in correct location in bitmap buffer
-  //        9. call write function, passing it the transformed buffer
 
-  fileReadHelper.newBitMap(bmpCon.bmp);
+exports.invert = function(preparedArray){
+  preparedArray.forEach((val, i) => { //invert colors
+    preparedArray[i] = (255 - parseInt(val, 16)).toString(16);
+    if (preparedArray[i].length === 1) preparedArray[i] = '0' + preparedArray[i];
+  });
+  bmpCon.bmObj.transformedArray = preparedArray;
 };
+
+exports.greyscale = function(preparedArray) {
+  preparedArray.forEach((val, i) => { //grayscale colors
+    preparedArray[i] = Math.ceil((parseInt(val, 16))*0.7);
+    if (preparedArray[i] > 255) preparedArray[i] = 255;
+    preparedArray[i] = preparedArray[i].toString(16);
+    if (preparedArray[i].length === 1) preparedArray[i] = '0' + preparedArray[i];
+  });
+  bmpCon.bmObj.transformedArray = preparedArray;
+};
+
+exports.blue = function(preparedArray) {
+  for (let i = 2; i <= preparedArray.length - 4; i += 4) { //change blue values
+    preparedArray[i] = Math.floor((parseInt(preparedArray[i], 16))*1.7);
+    if (preparedArray[i] > 255) preparedArray[i] = 255;
+    preparedArray[i] = preparedArray[i].toString(16);
+    if (preparedArray[i].length === 1) preparedArray[i] = '0' + preparedArray[i];
+  }
+  bmpCon.bmObj.transformedArray = preparedArray;
+};
+
+// function packageArray (transformedArray) {
+//   var packagedArray = [];
+//   for (let i = 0; i <= transformedArray.length - 4; i += 4) {
+//     packagedArray.push(transformedArray[i] + transformedArray[i+1] + transformedArray[i+2]);
+//   }
+//   var offset = 66;
+//   for (let i = 0; i < packagedArray.length; i++) {
+//     bmpCon.transformedBitMap.write(packagedArray[i], offset, 'hex');
+//     offset += 4;
+//   }
+// }
+
+
+// fileReadHelper.newBitMap(bmpCon.bmp);
+
+
+
 
 // function convertHexToBin (num) {
 //     console.log(parseInt(num, 16).toString(2))
